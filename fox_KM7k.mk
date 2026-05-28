aad711350012132877548013dfe1a46bc97329d7
Novicio+MillenniumTeam <157868622+Novicio-2309@users.noreply.github.com>
2026-05-28 23:00:07 +0800

    Add initial configuration for OrangeFox Recovery

---
+++ fox_KM7k.mk
@@ -0,0 +1,14 @@
+# Copyright (C) 2025-2026 OrangeFox Recovery Project
+# Copyright (C) 2026 Novicio2309
+# SPDX-License-Identifier: GPL-3.0-only
+
+# OrangeFox maintainer
+OF_MAINTAINER := Novicio301129
+
+# Screen settings
+OF_SCREEN_H := 2400
+OF_STATUS_H := 100
+OF_STATUS_INDENT_LEFT := 48
+OF_STATUS_INDENT_RIGHT := 48
+OF_ALLOW_DISABLE_NAVBAR := 0
+OF_CLOCK_POS := 1
